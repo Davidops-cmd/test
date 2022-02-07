3eeb3c63b4a949543e56010c437fd3f3bdd33ff0
David Ben Daniel <dariddan@gmail.com>
2022-02-08 00:51:00 +0200

    fix2

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,12 +3,12 @@ from add import add
 
 
 class TestBasic(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2,2), 4)
-    def test_add_big_numbers(self):
-        self.assertEqual(add(100,100), 200)
-    def test_fail(self):
-	self.assertEqual(add(150,100), 210)
-print("Hello"                    +"goodbye")
+	def test_add(self):
+        	self.assertEqual(add(2,2), 4)
+	def test_add_big_numbers(self):
+        	self.assertEqual(add(100,100), 200)
+	def test_fail(self):
+		self.assertEqual(add(150,100), 210)
+	print("Hello"                    +"goodbye")
 
 
